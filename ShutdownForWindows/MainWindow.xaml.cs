8e75f9e55329be805a205eff0680f436ee9e4fe3
Leapense <iso9661@gmail.com>
2022-05-14 18:49:51 +0900

    Change some ui for more sightable

--- ShutdownForWindows/MainWindow.xaml.cs
+++ ShutdownForWindows/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -27,6 +28,12 @@ namespace ShutdownForWindows
         public MainWindow()
         {
             InitializeComponent();
+
+            //string getCurrentCulture = null;
+
+            OSVer.Content = "        " + Environment.OSVersion;
+            OSVer.FontSize = 14;
+            
         }
         private void ListView_Selected(object sender, RoutedEventArgs e)
         {
@@ -62,6 +69,81 @@ namespace ShutdownForWindows
             {
                 Process.Start("taskmgr.exe");
                 this.Close();
+            }
+        }
+        [DllImport("PowrProf.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetSuspendState(false, true, true);
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Process[] localAll = Process.GetProcesses();
+            if(localAll.Length == 0)
+            {
+                Process.Start("shutdown", "/s /t 0");
+                this.Close();
+            }
+            else
+            {
+                dialog.Content = "Before you shutdown, Please check" + Environment.NewLine + "your unsaved documents or works!";
+                dialog.FontSize = 20;
+                dialog.ButtonLeftName = "Ok. I'll check.";
+                dialog.Visibility = Visibility.Visible;
+                dialog.ButtonRightName = "I saved all works.";
+                dialog.ButtonLeftClick += Dialog_ButtonLeftClick;
+                dialog.ButtonRightClick += Dialog_ButtonRightClick;
+                
+                dialog.Show();
+                
+            }
+        }
+
+        private void Dialog_ButtonRightClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("shutdown", "/s /t 0");
+            this.Close();
+        }
+
+        private void Dialog_ButtonLeftClick(object sender, RoutedEventArgs e)
+        {
+            dialog.Hide();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Process[] localAll = Process.GetProcesses();
+            if (localAll.Length == 0)
+            {
+                Process.Start("shutdown", "/r /t 0");
+                this.Close();
+            }
+            else
+            {
+                dialog.Content = "Before you restart, Please check" + Environment.NewLine + "your unsaved documents or works!";
+                dialog.FontSize = 20;
+                dialog.ButtonLeftName = "Ok. I'll check.";
+                dialog.Visibility = Visibility.Visible;
+                dialog.ButtonRightName = "I saved all works.";
+                dialog.ButtonLeftClick += Dialog_ButtonLeftClick;
+                dialog.ButtonRightClick += Dialog_ButtonRightClick;
+                dialog.Show();
+
+            }
+        }
+
+        private void MicaWindow_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                OSVer.FontSize = 20;
+            }
+            else if(this.WindowState == WindowState.Normal)
+            {
+                OSVer.FontSize = 14;
             }
         }
     }
