d9681cd919e9230d239abbb4e3e8e61e2ea1d5cb
Leapense <Leapense@sillain.ac.kr>
2022-05-20 09:58:00 +0900

    Add Theme Change

--- ShutdownForWindows/MainWindow.xaml.cs
+++ ShutdownForWindows/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MicaWPF.Controls;
+using MicaWPF.Services;
 using WPFUI.Controls;
 
 namespace ShutdownForWindows
@@ -81,30 +82,26 @@ namespace ShutdownForWindows
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Process[] localAll = Process.GetProcesses();
-            if(localAll.Length == 0)
-            {
-                Process.Start("shutdown", "/s /t 0");
-                this.Close();
-            }
-            else
-            {
-                dialog.Content = "Before you shutdown, Please check" + Environment.NewLine + "your unsaved documents or works!";
-                dialog.FontSize = 20;
-                dialog.ButtonLeftName = "Ok. I'll check.";
-                dialog.Visibility = Visibility.Visible;
-                dialog.ButtonRightName = "I saved all works.";
-                dialog.ButtonLeftClick += Dialog_ButtonLeftClick;
-                dialog.ButtonRightClick += Dialog_ButtonRightClick;
-                
-                dialog.Show();
-                
-            }
+            dialog.Content = "Before you shutdown, Please check" + Environment.NewLine + "your unsaved documents or works!";
+            dialog.FontSize = 20;
+            dialog.ButtonLeftName = "Ok. I'll check.";
+            dialog.Visibility = Visibility.Visible;
+            dialog.ButtonRightName = "I saved all works.";
+            dialog.ButtonLeftClick += Dialog_ButtonLeftClick;
+            dialog.ButtonRightClick += Dialog_ButtonRightClick;
+
+            dialog.Show();
         }
 
         private void Dialog_ButtonRightClick(object sender, RoutedEventArgs e)
         {
             Process.Start("shutdown", "/s /t 0");
+            this.Close();
+        }
+
+        private void Dialog_ButtonRightClick2(object sender, RoutedEventArgs e)
+        {
+            Process.Start("shutdown", "/r /t 0");
             this.Close();
         }
 
@@ -115,24 +112,14 @@ namespace ShutdownForWindows
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            Process[] localAll = Process.GetProcesses();
-            if (localAll.Length == 0)
-            {
-                Process.Start("shutdown", "/r /t 0");
-                this.Close();
-            }
-            else
-            {
-                dialog.Content = "Before you restart, Please check" + Environment.NewLine + "your unsaved documents or works!";
-                dialog.FontSize = 20;
-                dialog.ButtonLeftName = "Ok. I'll check.";
-                dialog.Visibility = Visibility.Visible;
-                dialog.ButtonRightName = "I saved all works.";
-                dialog.ButtonLeftClick += Dialog_ButtonLeftClick;
-                dialog.ButtonRightClick += Dialog_ButtonRightClick;
-                dialog.Show();
-
-            }
+            dialog.Content = "Before you restart, Please check" + Environment.NewLine + "your unsaved documents or works!";
+            dialog.FontSize = 20;
+            dialog.ButtonLeftName = "Ok. I'll check.";
+            dialog.Visibility = Visibility.Visible;
+            dialog.ButtonRightName = "I saved all works.";
+            dialog.ButtonLeftClick += Dialog_ButtonLeftClick;
+            dialog.ButtonRightClick += Dialog_ButtonRightClick2;
+            dialog.Show();
         }
 
         private void MicaWindow_StateChanged(object sender, EventArgs e)
@@ -145,6 +132,20 @@ namespace ShutdownForWindows
             {
                 OSVer.FontSize = 14;
             }
+        }
+
+        private void ThemeChanger_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var themeService = ThemeService.GetCurrent();
+            var tag_num = ThemeChanger.SelectedIndex;
+            if (tag_num == 1)
+                themeService.EnableMica(this, MicaWPF.BackdropType.Acrylic);
+            else if (tag_num == 2)
+                themeService.EnableMica(this, MicaWPF.BackdropType.Mica);
+            else if (tag_num == 3)
+                themeService.EnableMica(this, MicaWPF.BackdropType.Tabbed);
+            else if (tag_num == 4)
+                themeService.EnableMica(this, MicaWPF.BackdropType.None);
         }
     }
 }
